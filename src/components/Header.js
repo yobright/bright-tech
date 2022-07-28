d2ec0c18448d4b66c9b1a520cdbd1ffe87a8aa8b
kasskoo <kasskoo@kkghost.kkghost>
2022-07-28 11:06:28 +0200

    header ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import { Disclosure } from "@headlessui/react";
 import { Link } from "react-router-dom";
-
-const navigation = [{ name: "Contact", href: "/contact", current: false }];
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+const navigation = [
+  {
+    name: "À propos de nous", href: '/about', current: false
+  },
+  { name: "Services", href: "/services", current: false },
+  {
+    name: "Clients", href: "/work", current: false
+  }, 
+  {
+    name: "Contact",
+    href: "/contact",
+    current: false,
+  },
+];
 
 export default function Header() {
   return (
@@ -12,15 +25,18 @@ export default function Header() {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute flex ml-4 sm:hidden">
                 {/* Mobile menu button*/}
-                {navigation.map((item) => (
-                  <Link
-                    to={item.href}
-                    key={item.name}
-                    className="text-gray-900 hover:bg-gray-200 hover:text-gray-900 px-2 py-1 rounded-md text-xs font-medium uppercase cursor-pointer"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center ">
@@ -53,7 +69,6 @@ export default function Header() {
                   </div>
                 </div>
               </div>
-                      
             </div>
           </div>
         </>
